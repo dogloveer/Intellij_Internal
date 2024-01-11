2a28bced6f5a4362cb72e3f02fcdb3d91b75f8d9
Ala Drozdz <alcia2604@gmail.com>
2024-01-11 01:19:09 +0100

    summary-window

--- OOP/src/OgInternal/WindowSummary.java
+++ OOP/src/OgInternal/WindowSummary.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class WindowSummary {
     private JPanel mainPanel;
+    private JLabel TrainingType;
+    private JLabel TrainingTime;
+    private JLabel TrainingTrainer;
+    private JLabel TrainingMaterials;
+    private JButton OKButton;
 }
