376afe05c45eb8305401394be16e38109de9970e
dogloveer <152099188+dogloveer@users.noreply.github.com>
2024-01-11 01:33:09 +0100

    summary-window (#4)

--- OOP/src/OgInternal/WindowSummary.java
+++ OOP/src/OgInternal/WindowSummary.java
@@ -1,12 +1,25 @@
 package OgInternal;
 
 import javax.swing.*;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.*;
 
-public class WindowSummary {
+public class WindowSummary extends JFrame{
     private JPanel mainPanel;
     private JLabel TrainingType;
     private JLabel TrainingTime;
     private JLabel TrainingTrainer;
     private JLabel TrainingMaterials;
     private JButton OKButton;
+    JFrame frame=new JFrame();
+
+    public WindowSummary(){
+        frame.setContentPane(mainPanel);
+        frame.setSize(900,500);
+        frame.setLocation(200,100);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
